27f5ae59d87b65c6ece97061ee70c5d147e19461
SebasCalo <sebastiancalderon5204@gmail.com>
2026-08-30 09:50:09 -0500

    se arreglo la desaparicion de .env

--- Backend livestock/src/config/conectionDB.js
+++ Backend livestock/src/config/conectionDB.js
@@ -1,26 +1,15 @@
-require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
 const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+        database = "livestock",
+        username = "root",
+        password = "1029145208",
     {
-        dialect: 'mysql',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+
+        dialect: "mysql",
+        host: "localhost",
+        port: 3306
     }
 );
 
 module.exports = db;
-
-// IMPORTANTE:
-// Cada desarrollador debe crear su propio archivo .env
-// dentro de la carpeta "Backend livestock" con estas variables para que funcione la db:
-//
-// DB_HOST=localhost
-// DB_USER=root
-// DB_PASSWORD=TU_CONTRASEÑA
-// DB_NAME=livestock
-//
-// No subir el archivo .env a GitHub.
